9a781e7fa9dac934e1763aca3e4c6fa98fe3b7ff
vvilliam312 <31401950+vvilliam312@users.noreply.github.com>
2019-09-18 09:00:08 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # paceCalc
+1. Gör en function som genererar h/m/s till sekunder
+2. Gör en variabel som heter kilometers
+3. gör inputs för tiden och buttons
+4. få inputsen att console.loga det värde som de har i sig
+5.
--- script/script.js
+++ script/script.js
@@ -1,1 +1,36 @@
-console.log("WORKS");
+// console.log("WORKS");
+
+var kilometers="";
+
+
+var h;
+var m;
+var s;
+
+
+
+
+
+function calc(){
+kilometers=document.getElementById("km").value; 
+h=document.getElementById("hour").value;
+m=document.getElementById("min").value;
+s=document.getElementById("sec").value;
+
+
+// console.log(time);
+score();
+}
+
+function score(){
+    console.log(kilometers +" kilometers");
+    console.log(h+" hours "+m  +" minutes "+ s +" seconds" );
+
+}
+
+
+
+
+
+
+
